769a157edc5ffbc33527488f8be51a2e02bbb615
rahat-495 <md8702687@gmail.com>
2025-03-29 16:17:33 +0600

    some middlewares are created at dist folde !

--- dist/app.js
+++ dist/app.js
@@ -16,6 +16,8 @@ const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
 const cookie_parser_1 = __importDefault(require("cookie-parser"));
 const routes_1 = __importDefault(require("./app/routes"));
+const globalErrorHandler_1 = __importDefault(require("./app/modules/middlewares/globalErrorHandler"));
+const notFound_1 = __importDefault(require("./app/modules/middlewares/notFound"));
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
 app.use((0, cookie_parser_1.default)());
@@ -24,4 +26,6 @@ app.use('/api/v1', routes_1.default);
 app.get('/', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     res.json({ message: "The second project server are running !", success: true });
 }));
+app.use(globalErrorHandler_1.default);
+app.use(notFound_1.default);
 exports.default = app;
