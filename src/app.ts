d1c6a7d07e4fe6a3d9b2c3117c83291602b3b951
rahat-495 <md8702687@gmail.com>
2025-03-29 15:43:15 +0600

    main routes are initialized !

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@
 import express from "express" ;
 import cors from "cors" ;
 import cookieParser from "cookie-parser";
+import router from "./app/routes";
 
 const app = express() ;
 
@@ -9,7 +10,7 @@ app.use(express.json()) ;
 app.use(cookieParser()) ;
 app.use(cors({origin : ['http://localhost:5173'] , credentials : true})) ;
 
-// app.use('/api/v1' , router) ;
+app.use('/api/v1' , router) ;
 
 app.get('/' , async (req , res) => {
     res.json({message : "The second project server are running !" , success : true}) ;
