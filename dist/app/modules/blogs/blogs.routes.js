c84dc7eb49b25934ec366c4713a863305292f2c0
rahat-495 <md8702687@gmail.com>
2025-03-29 22:14:08 +0600

    validating request middleware are created at dist folder !

--- dist/app/modules/blogs/blogs.routes.js
+++ dist/app/modules/blogs/blogs.routes.js
@@ -1,1 +1,7 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.blogRoutes = void 0;
+const express_1 = require("express");
+const router = (0, express_1.Router)();
+router.post('/create-blog');
+exports.blogRoutes = router;
--- dist/app/routes/index.js
+++ dist/app/routes/index.js
@@ -1,11 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
+const blogs_routes_1 = require("../modules/blogs/blogs.routes");
 const router = (0, express_1.Router)();
 const moduleRoutes = [
     {
-        path: "/",
-        route: router,
+        path: "/blogs",
+        route: blogs_routes_1.blogRoutes,
     }
 ];
 moduleRoutes.forEach((route) => router.use(route.path, route.route));
