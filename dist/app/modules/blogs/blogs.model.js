0e04b45345aa221eb847ee0690c25925495df537
rahat-495 <md8702687@gmail.com>
2025-04-01 11:36:57 +0600

    blogs inter face or model are done at dist folder !

--- dist/app/modules/blogs/blogs.model.js
+++ dist/app/modules/blogs/blogs.model.js
@@ -1,1 +1,51 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.blogsModel = void 0;
+const mongoose_1 = require("mongoose");
+const authorSchema = new mongoose_1.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    profilePicture: {
+        type: String,
+        default: "",
+    },
+});
+const blogSchema = new mongoose_1.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    author: {
+        type: authorSchema,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    tags: {
+        type: [String],
+        required: true,
+    },
+    likes: {
+        type: Number,
+        default: 0,
+    },
+    views: {
+        type: Number,
+        default: 0,
+    },
+    publishedDate: {
+        type: String,
+        required: true,
+    },
+}, {
+    timestamps: true,
+});
+exports.blogsModel = (0, mongoose_1.model)("blog", blogSchema);
